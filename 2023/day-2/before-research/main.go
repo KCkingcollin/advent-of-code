a8d83dc3619167752bdc9ba469fde7131968b3b0
KCkingcollin <kckingcollin99@gmail.com>
2024-06-22 06:12:09 -0400

    updated code

--- 2023/day-2/before-research/main.go
+++ 2023/day-2/before-research/main.go
@@ -35,7 +35,7 @@ func solveDay2Part1() (int, error) {
     for scanner.Scan() {
         line = scanner.Text()
         isPossible, num := findStingBackwards(line)
-        if isPossible == true && num >= 0 {
+        if isPossible && num > 0 {
         sum += num
         }
         fmt.Println(num)
@@ -44,40 +44,28 @@ func solveDay2Part1() (int, error) {
 }
 
 func findStingBackwards(input string) (bool, int) {
-    // find := strings.HasPrefix
-    var game bool
-    var IDs int
+    find := strings.HasPrefix
+    var good bool
+    var ID int
     for i := len(input)-1; i >= 0; i-- {
         endOfStr := min(len(input)-i, 5)
         numLocation := min(len(input)-i, 3)
         wordBlock := input[i : i+endOfStr]
         numBlock := input[i : i+numLocation]
+        if len(input)-5 >= i {
+            numBlock = input[i-3 : i-3+numLocation]
+        }
         if i+endOfStr <= len(input) {
-            if game, IDs = findDigit(numBlock,  wordBlock); game == true {
-                return game, IDs
+            good, ID = findDigit(numBlock, wordBlock)
+            if find(wordBlock, "red") || find(wordBlock, "green") || find(wordBlock, "blue") {
+                if !good {
+                    return false, -1
+                }
+            } else if find(wordBlock, ":") {
+                return good, ID
             }
-            // switch {
-            // case find(wordBlock, "red"):
-            //     color, _ := findDigit(numBlock, wordBlock)
-            //     if !color {
-            //         return false, -1
-            //     }
-            // case find(wordBlock, "green"):
-            //     color, _ := findDigit(numBlock, wordBlock)
-            //     if !color {
-            //         return false, -1
-            //     }
-            // case find(wordBlock, "blue"):
-            //     color, _ := findDigit(numBlock, wordBlock)
-            //     if !color {
-            //         return false, -1
-            //     }
-            // case find(wordBlock, ":"):
-            //     game, IDs = findDigit(numBlock, wordBlock)
-            // }
         }
     }
-    // fmt.Println("no words B: ", input)
     return false, -1
 }
 
@@ -86,7 +74,7 @@ func findDigit(numInput string, word string) (bool, int) {
     var numStr string
     var num int
     var err error
-    fmt.Println(numInput)
+    // fmt.Println(numInput)
     for _, r := range numInput {
         if unicode.IsDigit(r) {
             numStr += string(r)
@@ -101,22 +89,22 @@ func findDigit(numInput string, word string) (bool, int) {
     }
     switch {
     case find(word, "red"):
-        if num > 12 {
+        if 0 > num || num > 12 {
             return false, -1
         } else {
-            return true, num
+            return true, -1
         }
     case find(word, "green"):
-        if num > 13 {
+        if 0 > num || num > 13 {
             return false, -1
         } else {
-            return true, num
+            return true, -1
         }
     case find(word, "blue"):
-        if num > 14 {
+        if 0 > num || num > 14 {
             return false, -1
         } else {
-            return true, num
+            return true, -1
         }
     case find(word, ":"):
         return true, num
