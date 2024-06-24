8d786bb6f7991f70c47fd93579205000cc88889d
KCkingcollin <kckingcollin99@gmail.com>
2024-06-23 20:54:44 -0400

    updating code

--- 2023/day-2/after-research/main.go
+++ 2023/day-2/after-research/main.go
@@ -13,7 +13,9 @@ import (
 
 func main() {
     start := time.Now()
-    result, err := solveDay2Part1()
+    // result, err := solveDay2Part1()
+    result, err := solveDay2Part2()
+    // the problem was so similar that i don't think it makes sense to change too much
     if err != nil {
         fmt.Printf("Error solving puzzle: %s", err)
     }
@@ -21,6 +23,23 @@ func main() {
     end := time.Now()
     mainLoopTime := end.Sub(start)
     fmt.Printf("Main loop took %v to complete.\n", mainLoopTime)
+}
+
+func solveDay2Part2() (int, error) {
+    puzzle, err := os.Open("puzzle")
+    if err != nil {
+        fmt.Println("failed to open puzzle")
+        return -1, err
+    }
+    defer puzzle.Close()
+    scanner := bufio.NewScanner(puzzle)
+    var sum int
+    for scanner.Scan() {
+        line := strings.Split(scanner.Text(), ": ")
+        calc := checkStringPart2(line[1])
+        sum += calc
+    }
+    return sum, err
 }
 
 func solveDay2Part1() (int, error) {
@@ -35,7 +54,7 @@ func solveDay2Part1() (int, error) {
     for scanner.Scan() {
         line := strings.Split(scanner.Text(), ": ")
         gameID := getNum(strings.Split(line[0], "Game ")[1])        
-        isPossible := checkString(line[1])
+        isPossible := checkStringPart1(line[1])
         if isPossible {
             sum += gameID
         }
@@ -60,7 +79,12 @@ func getNum(input string) (int) {
     return num
 }
 
-func checkString(input string) (bool) {
+func checkStringPart1(input string) (bool) {
+    isMax := map[string]int {
+        "red": 12,
+        "green": 13,
+        "blue": 14,
+    }
     var split []string
     splitIn := strings.Split(input, "; ")
     for _, block := range splitIn {
@@ -68,8 +92,8 @@ func checkString(input string) (bool) {
     }
     for i, r := range split {
         numWord := strings.Split(r, " ")
-        // fmt.Println(numWord[0], numWord[1])
-        if exceedsMax(string(numWord[1]), getNum(numWord[0])) {
+        // removed exceedsMax function as it was not needed
+        if isMax[string(numWord[1])] < getNum(numWord[0]) {
             return false
             // if exceedsMax is true, then return a false bool
         } else if len(split)-1 <= i {
@@ -81,12 +105,27 @@ func checkString(input string) (bool) {
     // if exceedsMax is never false, then return a true bool
 }
 
-func exceedsMax(word string, num int) (bool) {
-    isMax := map[string]int {
-        "red": 12,
-        "green": 13,
-        "blue": 14,
+func checkStringPart2(input string) (int) {
+    maxValues := map[string]int {
+        "red": 0,
+        "green": 0,
+        "blue": 0,
     }
-    return num > isMax[word]
+    var split []string
+    splitIn := strings.Split(input, "; ")
+    for _, block := range splitIn {
+        split = append(split, strings.Split(block, ", ")...)
+    }
+    for i, r := range split {
+        numWord := strings.Split(r, " ")
+        // used the max function instead of the if statement for speed and simplicity
+        maxValues[numWord[1]] = max(maxValues[numWord[1]], getNum(numWord[0]))
+        if len(split)-1 <= i {
+            return maxValues["red"] * maxValues["green"] * maxValues["blue"]
+            // return the power set 
+        }
+    }
+    return -1
+    // if exceedsMax is never false, then return a true bool
 }
 
